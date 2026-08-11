a78839215e6baa6568a687f7df0b01371167e087
ZeusGriffin <zeegriffin123@gmail.com>
2026-08-11 00:15:46 -0500

    Replace repository with No Razors FLOSS app

--- src/core/Register.ts
+++
@@ -1,6 +0,0 @@
-export interface ThreeStartRegister {}
-
-export type RegisterField<
-	TKey extends string,
-	TFallback = unknown,
-> = TKey extends keyof ThreeStartRegister ? ThreeStartRegister[TKey] : TFallback;
--- src/core/utils/assert-defined.ts
+++
@@ -1,5 +0,0 @@
-export const assertDefined = <T>(value: T | null | undefined, name: string): T => {
-	if (!value)
-		throw new ReferenceError(`Attempted to access '${name}' before it was initialized.`);
-	return value;
-};
